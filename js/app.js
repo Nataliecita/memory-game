f3b3e58feb4af5c64f1654246fd02f1e5950c49e
Natalie Ruiz <natnatruiz@gmail.com>
2018-06-05 11:33:59 +0200

    Add template modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+// make congrats message 
+
+// 1 pop  up modal, when game is over
+// https://www.w3schools.com/howto/howto_css_modals.asp
+//2. display rating, moves and time
+// 3.ask if they want to play again. link the restard game or new game thing. 
+
+
+
 /*
  * Create a list that holds all of your cards
  */
@@ -331,5 +340,33 @@ reset.addEventListener("click", function(){
 
 	start();
 })
+
+
+// MODAL
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
